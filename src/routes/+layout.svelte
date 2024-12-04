8baef85851ba95f6f4a64c608090b2e7eb672d50
Nicklas Frahm <nicklas.frahm@gmail.com>
2024-12-05 00:39:37 +0100

    feat(website): create prototype using svelte kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<Navbar class="shadow dark:bg-gray-900">
+	<NavBrand href="/">
+		<img src="/favicon.png" class="me-3 h-6 sm:h-9 rounded" alt="nicklasfrahm.dev logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">nicklasfrahm.dev</span>
+	</NavBrand>
+	<NavHamburger />
+	<div class="flex align-center justify-between flex-grow ml-4">
+		<NavUl>
+			<NavLi href="/">Home</NavLi>
+		</NavUl>
+		<DarkMode class="self-center"/>
+	</div>
+</Navbar>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container mx-auto p-8">
+    <h1 class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Welcome to my personal website!
+    </h1>
+</div>
